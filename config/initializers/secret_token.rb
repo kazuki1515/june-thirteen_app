0e4a722214fb3d7724bc6b280c48b614ce4ea863
Kazuki <wa_1023@yahoo.co.jp>
2017-06-13 14:11:24 +0000

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JuneThirteenApp::Application.config.secret_key_base = 'bf5443aa812386c09004b222dbf0e0d550dc758122d4f089939521fbf7d31e0ff2d80b457f18b1c05b78f19b2ebaa05b64eeaf6ef9ace4c500c62ace13d415e9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JuneThirteenApp::Application.config.session_store :cookie_store, key: '_june-thirteen_app_session'
